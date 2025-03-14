3ac0d5b34dfc6004e7f4896d2201154b3b1249da
Thiên <thiennguyenhoang32@gmail.com>
2025-03-15 02:03:40 +0700

    Initial commit

---
+++ DeliveryMangementSystem/Datas/myDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+using dbShipper;
+
+
+public class myDbContext : DbContext
+{
+    public DbSet<Shipper> Shippers { get; set; }
+
+    public myDbContext() : base() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myDbContext"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ DeliveryMangementSystem/Models/Shipper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbShipper.Models
+{   
+    [Table ("SHIPPER")]
+    public class Shipper
+    {
+        [Key]
+        [StringLength (10)]
+        [Column ("Shipper_ID")]
+        public String ID { get; set; }
+        [StringLength (10)]
+        [Column ("Phone")] 
+        public String Phone { get; set; }
+        [StringLength (100)]
+        [Column ("Name")]
+        public String Name { get; set; }
+
+    }
+}
