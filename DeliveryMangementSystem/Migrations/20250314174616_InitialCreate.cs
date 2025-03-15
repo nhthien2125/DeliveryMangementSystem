992465064066a8a1c7f3eef91910056ded9333d7
Thiên <thiennguyenhoang32@gmail.com>
2025-03-15 17:46:16 +0700

    Repush Project

---
+++ DeliveryMangementSystem/Migrations/20250314174616_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeliveryMangementSystem.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SHIPPER",
+                columns: table => new
+                {
+                    Shipper_ID = table.Column<string>(maxLength: 10, nullable: false),
+                    Phone = table.Column<string>(maxLength: 10, nullable: true),
+                    Name = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SHIPPER", x => x.Shipper_ID);
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SHIPPER");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Pull về xong thì restore lại package
+Tools → NuGet Package Manager → Package Manager Console. 
+chạy lệnh nuget restore
