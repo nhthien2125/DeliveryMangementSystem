3e19463a3e837beb110c0f1fcfc9448099c1ee28
Thiên <thiennguyenhoang32@gmail.com>
2025-03-15 19:45:28 +0700

    Models added

---
+++ DeliveryMangementSystem/Models/Administrator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Administrator.Models
+{
+    [Table ("ADMINISTRATOR")]
+    class Administrator
+    {
+        [Key]
+        [StringLength (10)]
+        [Column ("Admin_ID")]
+        public string Id { get; set; }
+
+        [StringLength (50)]
+        [Column ("Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [StringLength (50)]
+        [Column ("Password")]
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
+//done
--- DeliveryMangementSystem/Models/Shipper.cs
+++ DeliveryMangementSystem/Models/Shipper.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace dbShipper.Models
 {   
     [Table ("SHIPPER")]
@@ -18,9 +13,11 @@ namespace dbShipper.Models
         [StringLength (10)]
         [Column ("Phone")] 
         public String Phone { get; set; }
+
         [StringLength (100)]
         [Column ("Name")]
         public String Name { get; set; }
-
     }
 }
+
+//done
