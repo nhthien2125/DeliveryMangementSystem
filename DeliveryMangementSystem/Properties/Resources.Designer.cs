6624a1586448791c6f6844367402f513a9492bc1
tuchmm <leacv494@gmail.com>
2025-03-28 13:26:11 +0700

    ccaa

--- DeliveryMangementSystem/Properties/Resources.Designer.cs
+++ DeliveryMangementSystem/Properties/Resources.Designer.cs
@@ -83,6 +83,36 @@ namespace DeliveryMangementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_default_account {
+            get {
+                object obj = ResourceManager.GetObject("icon_default_account", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_default_branch {
+            get {
+                object obj = ResourceManager.GetObject("icon_default_branch", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_default_logout {
+            get {
+                object obj = ResourceManager.GetObject("icon_default_logout", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logout {
             get {
                 object obj = ResourceManager.GetObject("logout", resourceCulture);
